3a7a47ebfb9e03517a5c174ac3836e8ddb506667
zhangzhuofei1 <z15903509849@163.com>
2025-04-03 11:00:56 +0800

    优化个人中心返回后不刷新图片

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,35 @@
 <script setup>
-import { ref } from "vue";
+import { ref , provide , nextTick } from 'vue'
 import { RouterView } from "vue-router";
 
 const screenHeight = ref(667 + "px"); // 页面高度
 
 // 重新设置页面 size
 const resetPageMinSize = () => {
-	screenHeight.value = window.innerHeight + "px";
+  screenHeight.value = window.innerHeight + "px";
 };
 
 window.addEventListener("resize", resetPageMinSize);
 window.addEventListener("DOMContentLoaded", resetPageMinSize);
 
+const isRouterActive = ref(true)
+const reload = () => {
+  isRouterActive.value = false
+  nextTick(() => {
+    isRouterActive.value = true
+  })
+}
+provide('reload', reload)
 </script>
 
 <template>
   <div id="app">
-    <RouterView :style="{ height: screenHeight }"></RouterView>
+    <RouterView :style="{ height: screenHeight }" v-if="isRouterActive"></RouterView>
   </div>
 </template>
 
 <style scoped>
-.app{
+.app {
   width: 100%;
   height: 100%;
 }
